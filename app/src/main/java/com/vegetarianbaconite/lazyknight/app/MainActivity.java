9affea98ea89a91af13d384a39d6fe5407193687
Dominic Canora <dominiccanora@gmail.com>
2019-08-20 19:24:40 -0400

    Update to AndroidX. Add support for night mode.

--- app/src/main/java/com/vegetarianbaconite/lazyknight/app/MainActivity.java
+++ app/src/main/java/com/vegetarianbaconite/lazyknight/app/MainActivity.java
@@ -1,12 +1,13 @@
 package com.vegetarianbaconite.lazyknight.app;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.util.Log;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.vegetarianbaconite.schedulelib.Lecture;
 import com.vegetarianbaconite.schedulelib.Schedules;
